d3a75259437580cabad439c6fd811d5cf70b5257
Andres <maaucay@sudamericano.edu.ec>
2023-11-22 16:25:44 -0500

    first commit

---
+++ src/main/kotlin/com/RG4LIFE/Factura/FacturaApplication.kt
@@ -0,0 +1,11 @@
+package com.RG4LIFE.Factura
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FacturaApplication
+
+fun main(args: Array<String>) {
+	runApplication<FacturaApplication>(*args)
+}
